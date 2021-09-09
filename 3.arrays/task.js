25f0d09cac921a04603af356adeff2c036765e36
Alex Plotnikow <plotnikovalex18@yandex.ru>
2021-09-09 21:39:41 +0300

    commitHwArray

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,14 +1,19 @@
 function compareArrays(arr1, arr2) {
   let result;
+  if (arr1.length !== arr2.length) return false;
+  arr1.every((item,idx) => {
+    result = item === arr2[idx]
+  });
 
   // Ваш код
+ 
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
+  let resultArr = arr.filter((item) => item >0).filter(item => (item%3===0)).map(item=>(item*10));
+  
   // Ваш код
 
   return resultArr; // array
